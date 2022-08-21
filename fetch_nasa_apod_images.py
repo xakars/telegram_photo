5fccbc5bce10cf9fd588d38d3d53bdadb7f85cac
Arsen <xakars02@gmail.com>
2022-08-21 12:18:30 +0300

    download photos from NASA

---
+++ fetch_nasa_apod_images.py
@@ -0,0 +1,33 @@
+import requests
+import os
+from dotenv import load_dotenv
+from useful_tools import save_image
+from useful_tools import get_extension_from_url
+
+
+def feth_nasa_daily_photo():
+	url = "https://api.nasa.gov/planetary/apod"
+	payload = {
+		'api_key': os.environ['NASA_TOKEN'],
+		'count': 30
+	}
+	response = requests.get(url, params=payload)
+	response.raise_for_status
+	all_img_url = []
+	response_as_json = response.json()
+
+	for response in response_as_json:
+		all_img_url.append(response.get('hdurl'))
+
+	for img_index, img_url in enumerate(all_img_url):
+		if not img_url:
+			continue
+		pic_name = 'nasa' + str(img_index) + get_extension_from_url(img_url)
+		save_image(img_url, 'images_nasa_apod', pic_name)
+
+def  main():
+	feth_nasa_daily_photo()
+
+if __name__ == '__main__':
+	load_dotenv()
+	main()
---
+++ fetch_nasa_epic_images.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+from useful_tools import save_image
+
+
+def feth_epic_photo():
+	url = "https://api.nasa.gov/EPIC/api/natural/all"
+	payload = {
+		'api_key': os.environ['NASA_TOKEN']
+	}
+	response = requests.get(url, params=payload)
+	response.raise_for_status
+	for response in response.json()[:5]:
+		date = response['date']
+		url = f"https://api.nasa.gov/EPIC/api/natural/date/{date}"
+		response = requests.get(url, params=payload)
+		response.raise_for_status
+		img_date = response.json()[0]['date'].split()[0]
+		img_date_for_url = img_date.replace('-', '/')
+		image = response.json()[0]['image']
+		img_url = f"https://api.nasa.gov/EPIC/archive/natural/{img_date_for_url}/png/{image}.png"
+		response = requests.get(img_url, params=payload)
+		response.raise_for_status
+		picname = image + '.png'
+		save_image(response.url, 'images_nasa_epic', picname)
+
+
+def main():
+	feth_epic_photo()
+
+if __name__ == '__main__':
+	load_dotenv()
+	main()
