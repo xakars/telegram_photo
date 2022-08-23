14a7c713ff9968a6959bf7d41be0f5c93a45fa32
Arsen <xakars02@gmail.com>
2022-08-23 16:35:52 +0300

    used with ... as

--- tg_bot.py
+++ tg_bot.py
@@ -6,19 +6,21 @@ import time
 from dotenv import load_dotenv
 
 
-def post_to_chanel(token: str, delay_time: int):
+def post_to_chanel(token: str, chat_id: str, delay_time: int):
     bot = telegram.Bot(token=token)
     dir_with_imgs = 'images'
     while True:
         images_name = os.listdir(dir_with_imgs)
         random_img = random.choice(images_name)
         path = os.path.join(dir_with_imgs, random_img)
-        bot.send_document(chat_id='@devmn_test', document=open(path, 'rb'))
+        with open(path, 'rb') as file:
+            bot.send_document(chat_id=chat_id, document=file.read())
         time.sleep(delay_time)
 
 
 def main():
     token = os.environ['TG_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
     parser = argparse.ArgumentParser()
     parser.add_argument('--delay_time', help='enter delay time in hours', type=int)
     args = parser.parse_args()
@@ -27,7 +29,7 @@ def main():
         delay_time = 4*3600
     else:
         delay_time *= 3600
-    post_to_chanel(token, delay_time)
+    post_to_chanel(token, chat_id, delay_time)
 
 if __name__ == '__main__':
     load_dotenv()
